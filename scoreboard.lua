563183463e4fc4881d974885899371bb6e827832
FSSynthetic <fssynthetic.dev@gmail.com>
2021-09-19 17:22:17 -0400

    Added Self Toggle Ability

--- scoreboard.lua
+++ scoreboard.lua
@@ -1,25 +1,12 @@
 local listOn = false
 local total
-local Toggled = false
-
-RegisterCommand("scoreboardToggle", function(source, args)
-    if args[1] == "on" then
-        Toggled = true
-        notify("~g~Scoreboard has been set to a toggle!")
-    elseif args[1] == "off" then
-        Toggled = false
-        notify("~g~Scoreboard has been set to a hold!")
-    else
-        notify("~o~Please to /scoreboardToggle on or off")
-    end
-end)
 
 Citizen.CreateThread(function()
     listOn = false
     while true do
         Wait(0)
 
-        if toggle() and GetLastInputMethod(0) then
+        if IsControlJustReleased(0, Config.KeyBind) and GetLastInputMethod(0) then
             if not listOn then
                 local players = {}
                 ptable = GetPlayers()
@@ -51,7 +38,7 @@ Citizen.CreateThread(function()
                     Wait(0)
                     DisableAllControlActions(0)
                     EnableControlAction(0, Config.KeyBind, true)
-                    if (IsControlJustReleased(0, Config.KeyBind)) then
+                    if(IsControlJustReleased(0, Config.KeyBind)) then
                         SetNuiFocus(false, false)
                         SetNuiFocusKeepInput(false)
                         listOn = false
@@ -65,14 +52,6 @@ Citizen.CreateThread(function()
         end
     end
 end)
-
-function toggle()
-    if Toggled then
-        return IsControlJustReleased(0, Config.KeyBind)
-    else
-        return IsControlPressed(0, Config.KeyBind)
-    end
-end
 
 
 function GetPlayers()
@@ -98,10 +77,4 @@ function sanitize(txt)
     return txt
         :gsub('[&<>\n]', replacements)
         :gsub(' +', function(s) return ' '..('&nbsp;'):rep(#s-1) end)
-end
-
-function notify(text)
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawNotification(true, true)
 end
